35290d95780010e9b52b2720adfa967e71d51bbf
hiddevdbos <hiddevandenbos@upcmail.nl>
2020-10-04 14:56:40 +0200

    indent retry

--- car.py
+++ car.py
@@ -4,138 +4,138 @@ from mesa import *
 
 class Car(Agent):
 
-	def __init__(self, id, model, direction):
-		super().__init__(id, model)     # required by mesa
-		self.type = 'car'
-		self.color = self.randomColor() # every car is a random color
-		self.direction = direction      # direction the car is travelling
-		self.distance = 0               # distance the car has travelled
-		
-	# remove agent from the grid if it  reached the end
-	def removeAgent(self):
-		self.model.schedule.remove(self)   # remove agent from schedule
-		self.model.grid.remove_agent(self) # remove agent from grid
+ def __init__(self, id, model, direction):
+  super().__init__(id, model)     # required by mesa
+  self.type = 'car'
+  self.color = self.randomColor() # every car is a random color
+  self.direction = direction      # direction the car is travelling
+  self.distance = 0               # distance the car has travelled
+  
+ # remove agent from the grid if it  reached the end
+ def removeAgent(self):
+  self.model.schedule.remove(self)   # remove agent from schedule
+  self.model.grid.remove_agent(self) # remove agent from grid
 
-	# check if car can/is allowed to move
-	def can_move(self, edge, x):
-		light = self.getTrafficLight() # determine relevant traffic light
-		if self.pos[x] == edge:        # if agent has reached end of the grid
-			self.removeAgent()
-			return False                  # car cannot move, as it is gone
-		if light.color == 'red' and (self.carAhead() or self.pos[x] == light.pos[x]):
-			return False                  # car cannot move forward    
-		return True                    # car can move forward
-	
-	# move agent 1 cell forward and increase travelled distance by one
-	def moveForward(self, x, y):
-		self.model.grid.move_agent(self, (self.pos[0] + x, self.pos[1] + y))
-		self.distance += 1 # increase travelled distance
+ # check if car can/is allowed to move
+ def can_move(self, edge, x):
+  light = self.getTrafficLight() # determine relevant traffic light
+  if self.pos[x] == edge:        # if agent has reached end of the grid
+   self.removeAgent()
+   return False                  # car cannot move, as it is gone
+  if light.color == 'red' and (self.carAhead() or self.pos[x] == light.pos[x]):
+   return False                  # car cannot move forward    
+  return True                    # car can move forward
+ 
+ # move agent 1 cell forward and increase travelled distance by one
+ def moveForward(self, x, y):
+  self.model.grid.move_agent(self, (self.pos[0] + x, self.pos[1] + y))
+  self.distance += 1 # increase travelled distance
 
 
-	# main move loop, determines if and where the car moves
-	def move(self):
-		if self.direction == 'east':                     # if car is moving east
-			if self.can_move(self.model.grid.width - 1, 0): # if the car can move
-				if self.turnAhead():                           # if car has to turn
-					if self.distance < self.model.grid.width / 2: 
-						self.turn('south', 0, -1)                    # car has to turn right
-					else:
-					 self.turn('north', 0, 1)                     # car has to turn left
-				else:
-					self.moveForward(1, 0)                        # the car moves forward
-		
-		# all the other 3 parts work the same, just with different variables
-		elif self.direction == 'west':
-			if self.can_move(0, 0):
-				if self.turnAhead():
-					if self.distance < self.model.grid.width / 2:	
-						self.turn('north', 0, 1)
-					else:
-					 self.turn('south', 0, -1)
-				else:
-					self.moveForward(-1, 0);
-		
-		elif self.direction == 'north':
-			if self.can_move(self.model.grid.width - 1, 1):
-				if self.turnAhead():
-					if self.distance < self.model.grid.width / 2:
-						self.turn('east', 1, 0)
-					else:
-					 self.turn('west', -1, 0)
-				else:
-					self.moveForward(0, 1);
-		
-		elif self.direction == 'south':
-			if self.can_move(0, 1):
-				if self.turnAhead():
-					if self.distance < self.model.grid.width / 2:
-						self.turn('west', -1, 0)
-					else:
-					 self.turn('east', 1, 0)
-				else:
-					self.moveForward(0, -1);
+ # main move loop, determines if and where the car moves
+ def move(self):
+  if self.direction == 'east':                     # if car is moving east
+   if self.can_move(self.model.grid.width - 1, 0): # if the car can move
+    if self.turnAhead():                           # if car has to turn
+     if self.distance < self.model.grid.width / 2: 
+      self.turn('south', 0, -1)                    # car has to turn right
+     else:
+      self.turn('north', 0, 1)                     # car has to turn left
+    else:
+     self.moveForward(1, 0)                        # the car moves forward
+  
+  # all the other 3 parts work the same, just with different variables
+  elif self.direction == 'west':
+   if self.can_move(0, 0):
+    if self.turnAhead():
+     if self.distance < self.model.grid.width / 2: 
+      self.turn('north', 0, 1)
+     else:
+      self.turn('south', 0, -1)
+    else:
+     self.moveForward(-1, 0);
+  
+  elif self.direction == 'north':
+   if self.can_move(self.model.grid.width - 1, 1):
+    if self.turnAhead():
+     if self.distance < self.model.grid.width / 2:
+      self.turn('east', 1, 0)
+     else:
+      self.turn('west', -1, 0)
+    else:
+     self.moveForward(0, 1);
+  
+  elif self.direction == 'south':
+   if self.can_move(0, 1):
+    if self.turnAhead():
+     if self.distance < self.model.grid.width / 2:
+      self.turn('west', -1, 0)
+     else:
+      self.turn('east', 1, 0)
+    else:
+     self.moveForward(0, -1);
 
-	# get the correct traffic light 
-	def getTrafficLight(self):
-		for light in self.model.traffic_lights: # loop over all lights
-			if light.direction == self.direction:  # light has to have same direction as car
-				if self.direction == 'east' or self.direction == 'west':
-					if light.pos[1] == self.pos[1]:      # light has to be in same lane as car
-						return light
-				if self.direction == 'north' or self.direction == 'south':
-					if light.pos[0] == self.pos[0]:      # light has to be in same lane as car
-						return light
+ # get the correct traffic light 
+ def getTrafficLight(self):
+  for light in self.model.traffic_lights: # loop over all lights
+   if light.direction == self.direction:  # light has to have same direction as car
+    if self.direction == 'east' or self.direction == 'west':
+     if light.pos[1] == self.pos[1]:      # light has to be in same lane as car
+      return light
+    if self.direction == 'north' or self.direction == 'south':
+     if light.pos[0] == self.pos[0]:     # light has to be in same lane as car
+      return light
 
-	# look at the contents of the cell 1 position ahead
-	def lookAhead(self):
-		if(self.direction == 'east'):
-			cellContents = list(self.model.grid.iter_cell_list_contents((self.pos[0]+1,self.pos[1])))
-		elif(self.direction == 'west'):
-			cellContents = list(self.model.grid.iter_cell_list_contents((self.pos[0]-1,self.pos[1])))
-		elif(self.direction == 'south'):
-			cellContents = list(self.model.grid.iter_cell_list_contents((self.pos[0],self.pos[1]-1)))
-		elif(self.direction == 'north'):
-			cellContents = list(self.model.grid.iter_cell_list_contents((self.pos[0],self.pos[1]+1)))
-		return cellContents
+ # look at the contents of the cell 1 position ahead
+ def lookAhead(self):
+  if(self.direction == 'east'):
+   cellContents = list(self.model.grid.iter_cell_list_contents((self.pos[0]+1,self.pos[1])))
+  elif(self.direction == 'west'):
+   cellContents = list(self.model.grid.iter_cell_list_contents((self.pos[0]-1,self.pos[1])))
+  elif(self.direction == 'south'):
+   cellContents = list(self.model.grid.iter_cell_list_contents((self.pos[0],self.pos[1]-1)))
+  elif(self.direction == 'north'):
+   cellContents = list(self.model.grid.iter_cell_list_contents((self.pos[0],self.pos[1]+1)))
+  return cellContents
 
-	# check whether the car has to make a turn
-	def turnAhead(self):
-		cellAhead = self.lookAhead()
-		for agent in cellAhead:
-			# if next cell is a barrier
-			if(agent.type == 'background' and agent.color == 'darkslategrey'):	
-				return True              # car has to make a turn
-		return False               # car does not have to make a turn
+ # check whether the car has to make a turn
+ def turnAhead(self):
+  cellAhead = self.lookAhead()
+  for agent in cellAhead:
+   # if next cell is a barrier
+   if(agent.type == 'background' and agent.color == 'darkslategrey'): 
+    return True              # car has to make a turn
+  return False               # car does not have to make a turn
 
-	# check whether there is a car in the cell ahead
-	def carAhead(self):
-		cellAhead = self.lookAhead()   # get content from cell ahead
-		for agent in cellAhead:        # loop over all agents in cell ahead
-			if(agent.type == 'car'):      # the agent is a car
-				return True                  # there is a car ahead
-		return False                   # there is no car ahead
+ # check whether there is a car in the cell ahead
+ def carAhead(self):
+  cellAhead = self.lookAhead()   # get content from cell ahead
+  for agent in cellAhead:        # loop over all agents in cell ahead
+   if(agent.type == 'car'):      # the agent is a car
+    return True                  # there is a car ahead
+  return False                   # there is no car ahead
 
-	# called every step
-	def step(self):
-		self.move()
-	
-	# general turn function
-	def turn(self, direction, x, y):
-		self.direction = direction    # change direction the car is moving in
-		self.moveForward(x,y)         # move one cell forward in new direction
+ # called every step
+ def step(self):
+  self.move()
+ 
+ # general turn function
+ def turn(self, direction, x, y):
+  self.direction = direction    # change direction the car is moving in
+  self.moveForward(x,y)         # move one cell forward in new direction
 
-	# selects random color, to make it easy to see different between cars
-	def randomColor(self):
-		r = random.randint(0,255)
-		g = random.randint(0,255)
-		b = random.randint(0,255)
-		return '#{:02x}{:02x}{:02x}'.format(r,g,b) # transform to HTML color code
+ # selects random color, to make it easy to see different between cars
+ def randomColor(self):
+  r = random.randint(0,255)
+  g = random.randint(0,255)
+  b = random.randint(0,255)
+  return '#{:02x}{:02x}{:02x}'.format(r,g,b) # transform to HTML color code
 
-	def getColor(self):
-		return self.color
+ def getColor(self):
+  return self.color
 
-	def getType(self):
-		return self.type
+ def getType(self):
+  return self.type
 
-	def getDirection(self):
-		return self.direction
+ def getDirection(self):
+  return self.direction
--- main.py
+++ main.py
@@ -10,57 +10,57 @@ from grid import *
 
 # Set direction in which the car(arrow) points
 def setArrowDirection(agent, portrayal):
-	if agent.getDirection() == 'north':
-		portrayal['heading_x'] = 0
-		portrayal['heading_y'] = 1
-	if agent.getDirection() == 'south':
-		portrayal['heading_x'] = 0
-		portrayal['heading_y'] = -1
-	if agent.getDirection() == 'west':
-		portrayal['heading_x'] = -1
-		portrayal['heading_y'] = 0
-	if agent.getDirection() == 'east':
-		portrayal['heading_x'] = 1
-		portrayal['heading_y'] = 0
+ if agent.getDirection() == 'north':
+  portrayal['heading_x'] = 0
+  portrayal['heading_y'] = 1
+ if agent.getDirection() == 'south':
+  portrayal['heading_x'] = 0
+  portrayal['heading_y'] = -1
+ if agent.getDirection() == 'west':
+  portrayal['heading_x'] = -1
+  portrayal['heading_y'] = 0
+ if agent.getDirection() == 'east':
+  portrayal['heading_x'] = 1
+  portrayal['heading_y'] = 0
 
 # set direction of the traffic light
 def setRectDirection(agent, portrayal):
-	if agent.getDirection() == 'south' or agent.getDirection() == 'north':
-		portrayal['w'] = 0.8
-		portrayal['h'] = 0.1
-	if agent.getDirection() == 'east' or agent.getDirection() == 'west':
-		portrayal['w'] = 0.1
-		portrayal['h'] = 0.8
+ if agent.getDirection() == 'south' or agent.getDirection() == 'north':
+  portrayal['w'] = 0.8
+  portrayal['h'] = 0.1
+ if agent.getDirection() == 'east' or agent.getDirection() == 'west':
+  portrayal['w'] = 0.1
+  portrayal['h'] = 0.8
 
 # portrayal of agent on the canvas
 def agent_portrayal(agent):
-	# if the agent is a car
-	if agent.getType() == 'car':
-		portrayal = {	'Shape': 'arrowHead', # agent has shape of an arrowhead
-								'Color': agent.getColor(),    # color us different for every agent
-								'Filled': 'true',
-								'Layer': 1,
-								'scale': 0.5,
-								}
-		setArrowDirection(agent, portrayal) # arrow points towards agents direction
-	# if the agent is a traffic light
-	if agent.getType() == 'light':
-		portrayal = {	'Shape': 'rect',      # agent has a rectangle shape
-								'Color': agent.getColor(),    # color depends on the agent
-								'Filled': 'true',
-								'Layer': 2,                   # highest layer, so always visible
-								}
-		setRectDirection(agent, portrayal)  # set direction of the light
-	#if the agent is part of the background
-	if agent.getType() == 'background':
-		portrayal = {	'Shape': 'rect',      # shape is a square
-								'Color': agent.getColor(),
-								'Filled': 'true',
-								'Layer': 0,                   # is lowest layer, so only background
-								'w': 1,                       # fills entire block
-								'h': 1	                       # fills entire block
-								}
-	return portrayal
+ # if the agent is a car
+ if agent.getType() == 'car':
+  portrayal = { 'Shape': 'arrowHead', # agent has shape of an arrowhead
+        'Color': agent.getColor(),    # color us different for every agent
+        'Filled': 'true',
+        'Layer': 1,
+        'scale': 0.5,
+        }
+  setArrowDirection(agent, portrayal) # arrow points towards agents direction
+ # if the agent is a traffic light
+ if agent.getType() == 'light':
+  portrayal = { 'Shape': 'rect',      # agent has a rectangle shape
+        'Color': agent.getColor(),    # color depends on the agent
+        'Filled': 'true',
+        'Layer': 2,                   # highest layer, so always visible
+        }
+  setRectDirection(agent, portrayal)  # set direction of the light
+ #if the agent is part of the background
+ if agent.getType() == 'background':
+  portrayal = { 'Shape': 'rect',      # shape is a square
+        'Color': agent.getColor(),
+        'Filled': 'true',
+        'Layer': 0,                   # is lowest layer, so only background
+        'w': 1,                       # fills entire block
+        'h': 1                        # fills entire block
+        }
+ return portrayal
 
 # sliders that give possibility to change the traffic flow from specific direction
 east_flow = UserSettableParameter('slider', "% traffic flow from East", 15, 1, 100, 5)
@@ -73,10 +73,10 @@ canvas = CanvasGrid(agent_portrayal, 25, 25, 750, 750)
 
 # which grid and canvas to run
 server = ModularServer(Grid, [canvas], 'Traffic Flow Model', model_params= {"east_flow": east_flow, 
-																			"west_flow": west_flow, 
-																			"south_flow": south_flow, 
-																			"north_flow": north_flow})
-																			
+                   "west_flow": west_flow, 
+                   "south_flow": south_flow, 
+                   "north_flow": north_flow})
+                   
 
 server.port = 8523 # 8521 is default
 server.launch()
