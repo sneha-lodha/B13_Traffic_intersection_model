3875e1a65bd000bbe8b19904f02fc826ba830434
hiddevdbos <hiddevandenbos@upcmail.nl>
2020-10-30 13:01:26 +0100

    forgot to swap some tabs to spaces:

--- car.py
+++ car.py
@@ -4,166 +4,166 @@ from mesa import *
 
 
 class Car(Agent):
-	"""The car class manages all the decision made by the individual cars.
-	Most of the code of this class is related to the move function, which
-	determines whether the car can move."""
+ """The car class manages all the decision made by the individual cars.
+ Most of the code of this class is related to the move function, which
+ determines whether the car can move."""
 
-	def __init__(self, id, model, direction):
-		super().__init__(id, model)     # required by mesa
-		self.type = 'car'
-		self.color = self.randomColor()
-		self.direction = direction      # direction the car is travelling
-		self.distance = 0               # distance the car has travelled
-		self.wait_time = 0
+ def __init__(self, id, model, direction):
+  super().__init__(id, model)     # required by mesa
+  self.type = 'car'
+  self.color = self.randomColor()
+  self.direction = direction      # direction the car is travelling
+  self.distance = 0               # distance the car has travelled
+  self.wait_time = 0
 
-	def remove_agent(self):
-		"""Remove agent from the grid if it reached the end"""
-		self.model.schedule.remove(self)
-		self.model.grid.remove_agent(self)
+ def remove_agent(self):
+  """Remove agent from the grid if it reached the end"""
+  self.model.schedule.remove(self)
+  self.model.grid.remove_agent(self)
 
-	def can_move(self, edge, x):
-		"""Check if a car can/is allowed to move"""
-		light = self.getTrafficLight()  # determine relevant traffic light
-		if self.pos[x] == edge:        # if agent has reached end of the grid
-			self.remove_agent()
-			return False                  # car cannot move, as it is gone
-		if light.color == 'red' and (
-				self.carAhead() or self.pos[x] == light.pos[x]):
-			return False                  # car cannot move forward
-		return True                    # car can move forward
+ def can_move(self, edge, x):
+  """Check if a car can/is allowed to move"""
+  light = self.getTrafficLight()  # determine relevant traffic light
+  if self.pos[x] == edge:        # if agent has reached end of the grid
+   self.remove_agent()
+   return False                  # car cannot move, as it is gone
+  if light.color == 'red' and (
+    self.carAhead() or self.pos[x] == light.pos[x]):
+   return False                  # car cannot move forward
+  return True                    # car can move forward
 
-	def move_forward(self, x, y):
-		"""Move agent 1 cell forward and increase travelled distance by one"""
-		self.model.grid.move_agent(self, (self.pos[0] + x, self.pos[1] + y))
-		self.distance += 1
+ def move_forward(self, x, y):
+  """Move agent 1 cell forward and increase travelled distance by one"""
+  self.model.grid.move_agent(self, (self.pos[0] + x, self.pos[1] + y))
+  self.distance += 1
 
-	def move(self):
-		""" Main move loop, determines if and where the car moves """
-		if self.direction == 'east':
-			if self.can_move(self.model.grid.width - 1, 0):
-				if self.turn_ahead():                           # if car has to turn
-					if self.distance < self.model.grid.width / 2:
-						# car has to turn right
-						self.turn('south', 0, -1)
-					else:
-						# car has to turn left
-						self.turn('north', 0, 1)
-				else:
-					self.move_forward(1, 0)
-			else:
-				self.wait_time += 1
+ def move(self):
+  """ Main move loop, determines if and where the car moves """
+  if self.direction == 'east':
+   if self.can_move(self.model.grid.width - 1, 0):
+    if self.turn_ahead():                           # if car has to turn
+     if self.distance < self.model.grid.width / 2:
+      # car has to turn right
+      self.turn('south', 0, -1)
+     else:
+      # car has to turn left
+      self.turn('north', 0, 1)
+    else:
+     self.move_forward(1, 0)
+   else:
+    self.wait_time += 1
 
-		# all the other 3 parts work the same, just with different variables
-		elif self.direction == 'west':
-			if self.can_move(0, 0):
-				if self.turn_ahead():
-					if self.distance < self.model.grid.width / 2:
-						self.turn('north', 0, 1)
-					else:
-						self.turn('south', 0, -1)
-				else:
-					self.move_forward(-1, 0)
-			else:
-				self.wait_time += 1
+  # all the other 3 parts work the same, just with different variables
+  elif self.direction == 'west':
+   if self.can_move(0, 0):
+    if self.turn_ahead():
+     if self.distance < self.model.grid.width / 2:
+      self.turn('north', 0, 1)
+     else:
+      self.turn('south', 0, -1)
+    else:
+     self.move_forward(-1, 0)
+   else:
+    self.wait_time += 1
 
-		elif self.direction == 'north':
-			if self.can_move(self.model.grid.width - 1, 1):
-				if self.turn_ahead():
-					if self.distance < self.model.grid.width / 2:
-						self.turn('east', 1, 0)
-					else:
-						self.turn('west', -1, 0)
-				else:
-					self.move_forward(0, 1)
-			else:
-				self.wait_time += 1
+  elif self.direction == 'north':
+   if self.can_move(self.model.grid.width - 1, 1):
+    if self.turn_ahead():
+     if self.distance < self.model.grid.width / 2:
+      self.turn('east', 1, 0)
+     else:
+      self.turn('west', -1, 0)
+    else:
+     self.move_forward(0, 1)
+   else:
+    self.wait_time += 1
 
-		elif self.direction == 'south':
-			if self.can_move(0, 1):
-				if self.turn_ahead():
-					if self.distance < self.model.grid.width / 2:
-						self.turn('west', -1, 0)
-					else:
-						self.turn('east', 1, 0)
-				else:
-					self.move_forward(0, -1)
-			else: 
-				self.wait_time += 1
+  elif self.direction == 'south':
+   if self.can_move(0, 1):
+    if self.turn_ahead():
+     if self.distance < self.model.grid.width / 2:
+      self.turn('west', -1, 0)
+     else:
+      self.turn('east', 1, 0)
+    else:
+     self.move_forward(0, -1)
+   else: 
+    self.wait_time += 1
 
-	def getTrafficLight(self):
-		"""Get the correct traffic light"""
-		for light in self.model.traffic_lights:  # loop over all lights
-			if light.direction == self.direction:  # light has to have same direction as car
-				if self.direction == 'east' or self.direction == 'west':
-					# light has to be in same lane as car
-					if light.pos[1] == self.pos[1]:
-						return light
-				if self.direction == 'north' or self.direction == 'south':
-					# light has to be in same lane as car
-					if light.pos[0] == self.pos[0]:
-						return light
+ def getTrafficLight(self):
+  """Get the correct traffic light"""
+  for light in self.model.traffic_lights:  # loop over all lights
+   if light.direction == self.direction:  # light has to have same direction as car
+    if self.direction == 'east' or self.direction == 'west':
+     # light has to be in same lane as car
+     if light.pos[1] == self.pos[1]:
+      return light
+    if self.direction == 'north' or self.direction == 'south':
+     # light has to be in same lane as car
+     if light.pos[0] == self.pos[0]:
+      return light
 
-	def lookAhead(self):
-		"""Look at the contents of the cell 1 position ahead"""
-		if(self.direction == 'east'):
-			cellContents = list(
-				self.model.grid.iter_cell_list_contents(
-					(self.pos[0] + 1, self.pos[1])))
-		elif(self.direction == 'west'):
-			cellContents = list(
-				self.model.grid.iter_cell_list_contents(
-					(self.pos[0] - 1, self.pos[1])))
-		elif(self.direction == 'south'):
-			cellContents = list(
-				self.model.grid.iter_cell_list_contents(
-					(self.pos[0], self.pos[1] - 1)))
-		elif(self.direction == 'north'):
-			cellContents = list(
-				self.model.grid.iter_cell_list_contents(
-					(self.pos[0], self.pos[1] + 1)))
-		return cellContents
+ def lookAhead(self):
+  """Look at the contents of the cell 1 position ahead"""
+  if(self.direction == 'east'):
+   cellContents = list(
+    self.model.grid.iter_cell_list_contents(
+     (self.pos[0] + 1, self.pos[1])))
+  elif(self.direction == 'west'):
+   cellContents = list(
+    self.model.grid.iter_cell_list_contents(
+     (self.pos[0] - 1, self.pos[1])))
+  elif(self.direction == 'south'):
+   cellContents = list(
+    self.model.grid.iter_cell_list_contents(
+     (self.pos[0], self.pos[1] - 1)))
+  elif(self.direction == 'north'):
+   cellContents = list(
+    self.model.grid.iter_cell_list_contents(
+     (self.pos[0], self.pos[1] + 1)))
+  return cellContents
 
-	def turn_ahead(self):
-		"""Check whether the car has to make a turn"""
-		cellAhead = self.lookAhead()
-		for agent in cellAhead:
-			# if next cell is a barrier
-			if(agent.type == 'background' and agent.color == 'darkslategrey'):
-				return True              # car has to make a turn
-		return False               # car does not have to make a turn
+ def turn_ahead(self):
+  """Check whether the car has to make a turn"""
+  cellAhead = self.lookAhead()
+  for agent in cellAhead:
+   # if next cell is a barrier
+   if(agent.type == 'background' and agent.color == 'darkslategrey'):
+    return True              # car has to make a turn
+  return False               # car does not have to make a turn
 
-	def carAhead(self):
-		"""Check whether there is a car in the cell ahead"""
-		cellAhead = self.lookAhead()   # get content from cell ahead
-		for agent in cellAhead:
-			if(agent.type == 'car'):
-				return True                  # there is a car ahead
-		return False                   # there is no car ahead
+ def carAhead(self):
+  """Check whether there is a car in the cell ahead"""
+  cellAhead = self.lookAhead()   # get content from cell ahead
+  for agent in cellAhead:
+   if(agent.type == 'car'):
+    return True                  # there is a car ahead
+  return False                   # there is no car ahead
 
-	def step(self):
-		"""Called every step for every individual car"""
-		self.move()
+ def step(self):
+  """Called every step for every individual car"""
+  self.move()
 
-	def turn(self, direction, x, y):
-		"""General turn function"""
-		self.direction = direction    # change direction the car is moving in
-		# move one cell forward in new direction
-		self.move_forward(x, y)
+ def turn(self, direction, x, y):
+  """General turn function"""
+  self.direction = direction    # change direction the car is moving in
+  # move one cell forward in new direction
+  self.move_forward(x, y)
 
-	def randomColor(self):
-		"""Selects random color, to make it easy to see different between cars"""
-		colors = ['black', 'yellow', 'blue', 'brown', 'white'] 
-		x = random.randint(0, 3)
-		return colors[x - 1]
+ def randomColor(self):
+  """Selects random color, to make it easy to see different between cars"""
+  colors = ['black', 'yellow', 'blue', 'brown', 'white'] 
+  x = random.randint(0, 3)
+  return colors[x - 1]
 
-	def getColor(self):
-		"""Get the color of the car"""
-		return self.color
+ def getColor(self):
+  """Get the color of the car"""
+  return self.color
 
-	def getType(self):
-		"""Get the type of the car"""
-		return self.type
+ def getType(self):
+  """Get the type of the car"""
+  return self.type
 
-	def getDirection(self):
-		"""Get the direction of the car"""
-		return self.direction
+ def getDirection(self):
+  """Get the direction of the car"""
+  return self.direction
--- trafficlight.py
+++ trafficlight.py
@@ -4,187 +4,187 @@ from mesa import *
 
 
 class Traffic_light(Agent):
-	"""Class that is responsible for the functionality of the traffic
-	lights.
+ """Class that is responsible for the functionality of the traffic
+ lights.
 
-	Each traffic light has a direction and turn and it has a color so
-	it can change from red to green and vice versa. If traffic lights
-	are red, cars are not allowed to pass, whereas if they are green
-	cars are allowed to pass.
+ Each traffic light has a direction and turn and it has a color so
+ it can change from red to green and vice versa. If traffic lights
+ are red, cars are not allowed to pass, whereas if they are green
+ cars are allowed to pass.
 
-	In this class we test different methods on determining when the lights
-	change color.
-	"""
+ In this class we test different methods on determining when the lights
+ change color.
+ """
 
-	def __init__(self, id, model, direction, turn='no turn'):
-		super().__init__(id, model)
-		self.type = 'light'
-		self.color = 'red'
-		self.time = 0
-		self.direction = direction
-		self.turn = turn
-		self.demand = 0
-		self.waiting_time = 0
-		self.car_waiting = False
+ def __init__(self, id, model, direction, turn='no turn'):
+  super().__init__(id, model)
+  self.type = 'light'
+  self.color = 'red'
+  self.time = 0
+  self.direction = direction
+  self.turn = turn
+  self.demand = 0
+  self.waiting_time = 0
+  self.car_waiting = False
 
-	def timer1(self):
-		"""First timer that changes light in a brute force way, where
-		the times are already predetermined. The first naive approach
-		to the traffic light logic.
+ def timer1(self):
+  """First timer that changes light in a brute force way, where
+  the times are already predetermined. The first naive approach
+  to the traffic light logic.
 
-		The time variable is incremented and reset once it reaches 32 so
-		this runs in a loop fashion.
-		"""
-		if self.time == 5:
-			if self.direction == 'north':
-				self.setColor('red')
-		if self.time == 8:
-			if self.direction == 'east':
-				self.setColor('green')
-		if self.time == 13:
-			if self.direction == 'east':
-				self.setColor('red')
-		if self.time == 16:
-			if self.direction == 'south':
-				self.setColor('green')
-		if self.time == 21:
-			if self.direction == 'south':
-				self.setColor('red')
-		if self.time == 24:
-			if self.direction == 'west':
-				self.setColor('green')
-		if self.time == 29:
-			if self.direction == 'west':
-				self.setColor('red')
-		if self.time == 32:
-			if self.direction == 'north':
-				self.setColor('green')
-			self.time = 0
-		self.time += 1
+  The time variable is incremented and reset once it reaches 32 so
+  this runs in a loop fashion.
+  """
+  if self.time == 5:
+   if self.direction == 'north':
+    self.setColor('red')
+  if self.time == 8:
+   if self.direction == 'east':
+    self.setColor('green')
+  if self.time == 13:
+   if self.direction == 'east':
+    self.setColor('red')
+  if self.time == 16:
+   if self.direction == 'south':
+    self.setColor('green')
+  if self.time == 21:
+   if self.direction == 'south':
+    self.setColor('red')
+  if self.time == 24:
+   if self.direction == 'west':
+    self.setColor('green')
+  if self.time == 29:
+   if self.direction == 'west':
+    self.setColor('red')
+  if self.time == 32:
+   if self.direction == 'north':
+    self.setColor('green')
+   self.time = 0
+  self.time += 1
 
-	def timer2(self, times):
-		"""Timer that is semi-brute force.
+ def timer2(self, times):
+  """Timer that is semi-brute force.
 
-		This timer when called with the calculate_timer() in the grid.py
-		sets the correct intervals based on the flows of the traffic from
-		the different directions. Very similar to timer1() but times are
-		pre-calculated.
-		"""
-		if self.time == times[0]:
-			if self.direction == 'east':
-				self.setColor('green')
-		if self.time == times[1]:
-			if self.direction == 'east':
-				self.setColor('red')
-		if self.time == times[2]:
-			if self.direction == 'west':
-				self.setColor('green')
-		if self.time == times[3]:
-			if self.direction == 'west':
-				self.setColor('red')
-		if self.time == times[4]:
-			if self.direction == 'north':
-				self.setColor('green')
-		if self.time == times[5]:
-			if self.direction == 'north':
-				self.setColor('red')
-		if self.time == times[6]:
-			if self.direction == 'south':
-				self.setColor('green')
-		if self.time == times[7]:
-			if self.direction == 'south':
-				self.setColor('red')
-			self.time = 0
-		self.time += 1
-
-
-	def timer3(self):
-		"""Check with the controller whether the light should be green or red.
-		The 7 second pause assurres there are no collisions between cars"""
-		cell = list(self.model.grid.iter_cell_list_contents((0, 0)))
-		for agent in cell:
-			if (agent.type == 'controller'):
-				controller = agent
-		if self.direction == controller.green_lights and controller.time > 7:
-			self.setColor('green')
-			self.waiting_time = 0
-		else: 
-			self.setColor('red')
-
-	def calculate_demand(self):
-		"""Calculate the demand of the traffic light. The demand is calculated
-		as the amount of cars that are in the 10 cells in front of the light"""
-		self.demand = 0
-		self.car_waiting = False
-		for i in range(0,10):
-			if self.direction == 'east':
-				if self.car_present(self.pos[0] - i, self.pos[1]):
-					self.update_variables(i)
-			if self.direction == 'west':
-				if self.car_present(self.pos[0] + i, self.pos[1]):
-					self.update_variables(i)
-			if self.direction == 'south':
-				if self.car_present(self.pos[0], self.pos[1] + i):
-					self.update_variables(i)
-			if self.direction == 'north':
-				if self.car_present(self.pos[0], self.pos[1] - i):
-					self.update_variables(i)
-
-	def car_present(self, x, y):
-		"""Return whether there is a car 
-		at the location of the traffic light"""
-		cell = list(self.model.grid.iter_cell_list_contents((x, y)))
-		for agent in cell:
-			if (agent.type == 'car'):
-				return True
-		return False
-
-	def update_variables(self, i):
-		self.demand += 1
-		if i == 0:
-			self.car_waiting = True
-			self.waiting_time += 1
+  This timer when called with the calculate_timer() in the grid.py
+  sets the correct intervals based on the flows of the traffic from
+  the different directions. Very similar to timer1() but times are
+  pre-calculated.
+  """
+  if self.time == times[0]:
+   if self.direction == 'east':
+    self.setColor('green')
+  if self.time == times[1]:
+   if self.direction == 'east':
+    self.setColor('red')
+  if self.time == times[2]:
+   if self.direction == 'west':
+    self.setColor('green')
+  if self.time == times[3]:
+   if self.direction == 'west':
+    self.setColor('red')
+  if self.time == times[4]:
+   if self.direction == 'north':
+    self.setColor('green')
+  if self.time == times[5]:
+   if self.direction == 'north':
+    self.setColor('red')
+  if self.time == times[6]:
+   if self.direction == 'south':
+    self.setColor('green')
+  if self.time == times[7]:
+   if self.direction == 'south':
+    self.setColor('red')
+   self.time = 0
+  self.time += 1
 
 
-	def setColor(self, color):
-		"""Set the color od the light, either green or red."""
-		self.color = color
+ def timer3(self):
+  """Check with the controller whether the light should be green or red.
+  The 7 second pause assurres there are no collisions between cars"""
+  cell = list(self.model.grid.iter_cell_list_contents((0, 0)))
+  for agent in cell:
+   if (agent.type == 'controller'):
+    controller = agent
+  if self.direction == controller.green_lights and controller.time > 7:
+   self.setColor('green')
+   self.waiting_time = 0
+  else: 
+   self.setColor('red')
 
-	def getColor(self):
-		"""Get the current color of the light."""
-		return self.color
+ def calculate_demand(self):
+  """Calculate the demand of the traffic light. The demand is calculated
+  as the amount of cars that are in the 10 cells in front of the light"""
+  self.demand = 0
+  self.car_waiting = False
+  for i in range(0,10):
+   if self.direction == 'east':
+    if self.car_present(self.pos[0] - i, self.pos[1]):
+     self.update_variables(i)
+   if self.direction == 'west':
+    if self.car_present(self.pos[0] + i, self.pos[1]):
+     self.update_variables(i)
+   if self.direction == 'south':
+    if self.car_present(self.pos[0], self.pos[1] + i):
+     self.update_variables(i)
+   if self.direction == 'north':
+    if self.car_present(self.pos[0], self.pos[1] - i):
+     self.update_variables(i)
 
-	def getType(self):
-		"""Returns light as the type of the agent."""
-		return self.type
+ def car_present(self, x, y):
+  """Return whether there is a car 
+  at the location of the traffic light"""
+  cell = list(self.model.grid.iter_cell_list_contents((x, y)))
+  for agent in cell:
+   if (agent.type == 'car'):
+    return True
+  return False
 
-	def getDirection(self):
-		"""Returns the direction of the light."""
-		return self.direction
+ def update_variables(self, i):
+  self.demand += 1
+  if i == 0:
+   self.car_waiting = True
+   self.waiting_time += 1
 
-	def get_car_waiting(self):
-		return self.car_waiting
 
-	def get_direction(self):
-		return self.direction
+ def setColor(self, color):
+  """Set the color od the light, either green or red."""
+  self.color = color
 
-	def get_demand(self):
-		return self.demand
+ def getColor(self):
+  """Get the current color of the light."""
+  return self.color
 
-	def get_waiting_time(self):
-		return self.waiting_time
+ def getType(self):
+  """Returns light as the type of the agent."""
+  return self.type
 
-	def step(self):
-		"""Step function of the light called every time step.
+ def getDirection(self):
+  """Returns the direction of the light."""
+  return self.direction
 
-		Call either with timer1() or timer2() to see how the lights in the model
-		change colors.
-		"""
-		if self.model.system == 'Fixed time':
-			self.timer1()
-		if self.model.system == 'Flow based':
-			times = self.model.calculate_timer()
-			self.timer2(times)
-		if self.model.system == 'Demand based':
-			self.calculate_demand()
-			self.timer3()
+ def get_car_waiting(self):
+  return self.car_waiting
+
+ def get_direction(self):
+  return self.direction
+
+ def get_demand(self):
+  return self.demand
+
+ def get_waiting_time(self):
+  return self.waiting_time
+
+ def step(self):
+  """Step function of the light called every time step.
+
+  Call either with timer1() or timer2() to see how the lights in the model
+  change colors.
+  """
+  if self.model.system == 'Fixed time':
+   self.timer1()
+  if self.model.system == 'Flow based':
+   times = self.model.calculate_timer()
+   self.timer2(times)
+  if self.model.system == 'Demand based':
+   self.calculate_demand()
+   self.timer3()
